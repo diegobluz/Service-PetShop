a4088bcf339405500c402328e3213ae0feee0741
Diego Luz <dieguinholuz10@gmail.com>
2024-01-31 12:00:57 -0300

    Projeto Inicial

---
+++ src/main/java/com/grupo/service/petshop/ServicePetshopApplication.java
@@ -0,0 +1,13 @@
+package com.grupo.service.petshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicePetshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicePetshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo/service/petshop/ServicePetshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo.service.petshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicePetshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
